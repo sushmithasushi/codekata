2862b72408ef11025a6c271a5a783ae21d5e95c5
sushmithasushi <49743221+sushmithasushi@users.noreply.github.com>
2019-07-02 22:12:42 +0530

    Create primerange.py

---
+++ primerange.py
@@ -0,0 +1,10 @@
+x,y=map(int,input().split())
+for i in range(x,y+1):
+    if i>1:
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            print(i,end=" ")
+    else:
+        exit()
