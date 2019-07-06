7a2492cc6d0eb754bedb7b2b14a21eb5272f997a
sushmithasushi <49743221+sushmithasushi@users.noreply.github.com>
2019-07-06 23:27:33 +0530

    Update set87.py

--- set87.py
+++ set87.py
@@ -1,12 +1,13 @@
 ss=input()
 l=len(ss)
-x=l//2
 ll=list(ss)
-if x%2==0:
+x=l//2
+if l%2==0:
     ll[x]='*'
-    ll[x+1]='*'
+    ll[x-1]='*'
 else:
     ll[x]='*'
 for i in ll:
     print(i,end=" ")
     
+
