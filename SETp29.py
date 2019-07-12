7558efd51f126f93d6708b029ac805640be6e026
sushmithasushi <49743221+sushmithasushi@users.noreply.github.com>
2019-07-12 21:34:12 +0530

    Update SETp29.py

--- SETp29.py
+++ SETp29.py
@@ -1,8 +1,12 @@
 n=int(input())
+l=[]
 for i in range(2,n):
     c=0
     for j in range(2,i):
         if i%j==0:
             c+=1
     if c==0:
+        l.append(i)
+for i in l:
+    if n%i==0:
         print(i,end=" ")
