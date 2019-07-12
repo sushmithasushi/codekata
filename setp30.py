8f56e972cdf299acdaf1ee90566a6b8e6796b7ec
sushmithasushi <49743221+sushmithasushi@users.noreply.github.com>
2019-07-12 21:47:17 +0530

    Update setp30.py

--- setp30.py
+++ setp30.py
@@ -1,9 +1,9 @@
-inpt1=input()
-inpt2=''
-inpt3='0ABCDEFGHIJKLMNOPQRSTUVWXYZABC'
-for i in inpt1:
-    if i in inpt3:
-        inpt4=inpt3.index(i)
+x=input()
+y=''
+Z='0ABCDEFGHIJKLMNOPQRSTUVWXYZABC'
+for i in x:
+    if i in Z:
+        inpt4=Z.index(i)
         inpt4=inpt4+3
-        inpt2=inpt2+inpt3[inpt4]
-print(inpt2)
+        y=y+Z[inpt4]
+print(y)
